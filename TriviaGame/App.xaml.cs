27031c77ef0e6fadac25da2471cceac9412a8986
Aidan Nolan <aidan_nolan@icloud.com>
2026-09-19 00:45:33 -0500

    Add project files.

---
+++ TriviaGame/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TriviaGame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ TriviaGame/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TriviaGame")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TriviaGame.Pages")]
